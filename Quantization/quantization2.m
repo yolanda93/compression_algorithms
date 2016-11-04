4401e266f162e3e755f6f92922528333b3440d3e
Yolanda de la Hoz Simón <yolandadela.hoz.simon@alumnos.upm.es>
2016-11-04 16:51:02 +0100

    Create quantization2.m

---
+++ Quantization/quantization2.m
@@ -0,0 +1,13 @@
+function quantized_matrix = quantize_matrix(matrix, new_bytesize)
+quantized_matrix = zeros(size(matrix,1),size(matrix,2),'uint8');
+for row = 1:size(matrix,1)
+    for col = 1:size(matrix,2)
+        quantized_matrix(row,col) = quantize(matrix(row,col), new_bytesize);
+    end
+end
+end
+
+function quantization = quantize(pixel, new_bytesize)
+dif_levels = 2^(8-new_bytesize);
+quantization=floor(double(pixel)/dif_levels);
+end
