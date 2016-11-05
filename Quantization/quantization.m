ab6bb19ddc236191ac0dbe5cdbe93d41d8ca4226
Yolanda de la Hoz Simón <yolandadela.hoz.simon@alumnos.upm.es>
2016-11-05 12:04:38 +0100

    Update quantization.m

--- Quantization/quantization.m
+++ Quantization/quantization.m
@@ -7,10 +7,9 @@ out_image=zeros(size(input_image));
 
 for i =  1:size(input_image,1)
    for j = 1:size(input_image,2)
-         out_image(i,j) = round((input_image(i,j)/cluster_size-cluster_size)/2);
+         out_image(i,j) = (((round(input_image(i,j))/cluster_size)-cluster_size)/2);
    end
 end
 
 out_image = uint8(out_image);
 end
-
