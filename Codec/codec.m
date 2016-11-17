320e623d2d96131ba2fafc19dd31ba4ff2885cce
Ignacio Uyá Lasarte <i.uya.lasarte@gmail.com>
2016-11-17 13:05:52 +0100

    Codec finished (TD3)

--- Codec/codec.m
+++ Codec/codec.m
@@ -14,20 +14,20 @@ sub22 = lena_haar(257:512,1:256);
 sub23 = lena_haar(257:512,257:512);
 figure
 subplot(4,4,1)
-imshow(sub11,[0,255])
+imshow(sub11)
 subplot(4,4,2)
-imshow(sub12,[0,255])
+imshow(sub12)
 subplot(4,4,5)
-imshow(sub13,[0,255])
+imshow(sub13)
 subplot(4,4,6)
-imshow(sub14,[0,255])
+imshow(sub14)
 % Now for the bigger subbands we are going to use bigger subplots
 subplot(4,4,[3:4 7:8])
-imshow(sub21,[0,255])
+imshow(sub21)
 subplot(4,4,[9:10 13:14])
-imshow(sub22,[0,255])
+imshow(sub22)
 subplot(4,4,[11:12 15:16])
-imshow(sub23,[0,255])
+imshow(sub23)
 
 % We are going to use the same subplot distribution to show the histogram
 % of each sub image
@@ -57,42 +57,83 @@ imhist((sub23+255)./512)
 % we only have a uniform scalar quantizer, we will use that.
 % No Quantization for the ll part of the image (sub 11)
 
-
-
 % Need to count all the appearances of each number
 entropies = zeros(7,8);
-for i= 7:-1:1
-    q_sub11 = quantize_matrix(sub11,8);
-    q_sub12 = quantize_matrix(sub12,i);
-    q_sub13 = quantize_matrix(sub13,i);
-    q_sub14 = quantize_matrix(sub14,i);
-    q_sub21 = quantize_matrix(sub21,i);
-    q_sub22 = quantize_matrix(sub22,i);
-    q_sub23 = quantize_matrix(sub23,i);
-    e_sub11 = shannonEntropy(q_sub11);
-    e_sub12 = shannonEntropy(q_sub12);
-    e_sub13 = shannonEntropy(q_sub13);
-    e_sub14 = shannonEntropy(q_sub14);
-    e_sub21 = shannonEntropy(q_sub21);
-    e_sub22 = shannonEntropy(q_sub22);
-    e_sub23 = shannonEntropy(q_sub23);
-    q_lena = quantize_matrix(lena_haar,i);
-    q_lena(1:128,1:128) = q_sub11;
-    e_lena = shannonEntropy(q_lena);
-    dict = huffmanDict(q_lena);
-    sizeMap = containers.Map('KeyType','double', 'ValueType','any');
-    for row = 1:size(dict,1)
-        keyCell = dict(row,1);
-        valueCell = dict(row,2);
-        code = valueCell{1};
-        key = keyCell{1};
-        sizeMap(key) = size(code,2);
-    end
-    %SizeMap holds the lengths of all the symbols
-    % TODO subtitute the symbols by their length and add all the elements
-    % of the matrix, then divide by the nuelem of the matrix
-    entropies(i,:) = [e_sub11, e_sub12, e_sub13, e_sub14, e_sub21, e_sub22, e_sub23, e_lena];
-end 
+q_sub11 = sub11;
+q_sub12 = quantize_matrix(sub12,7);
+q_sub13 = quantize_matrix(sub13,7);
+q_sub14 = quantize_matrix(sub14,6);
+q_sub21 = quantize_matrix(sub21,5);
+q_sub22 = quantize_matrix(sub22,5);
+q_sub23 = quantize_matrix(sub23,4);
+
+e_sub11 = shannonEntropy(q_sub11);
+h_sub11 = numel(huffmanCode(q_sub11))/numel(q_sub11);
+
+e_sub12 = shannonEntropy(q_sub12);
+h_sub12 = numel(huffmanCode(q_sub12))/numel(q_sub12);
+
+e_sub13 = shannonEntropy(q_sub13);
+h_sub13 = numel(huffmanCode(q_sub13))/numel(q_sub13);
+
+e_sub14 = shannonEntropy(q_sub14);
+h_sub14 = numel(huffmanCode(q_sub14))/numel(q_sub14);
+
+e_sub21 = shannonEntropy(q_sub21);
+h_sub21 = numel(huffmanCode(q_sub21))/numel(q_sub21);
+
+e_sub22 = shannonEntropy(q_sub22);
+h_sub22 = numel(huffmanCode(q_sub22))/numel(q_sub22);
+
+e_sub23 = shannonEntropy(q_sub23);
+h_sub23 = numel(huffmanCode(q_sub23))/numel(q_sub23);
+
+% We need to "stitch" all the pieces of q_lena together
+q_lena(1:128,1:128) = q_sub11;
+q_lena(1:128,129:256) = q_sub12;
+q_lena(129:256,1:128) = q_sub13;
+q_lena(129:256,129:256) = q_sub14;
+q_lena(1:256,257:512) = q_sub21;
+q_lena(257:512,1:256) = q_sub22;
+q_lena(257:512,257:512) = q_sub23;
+e_lena = shannonEntropy(q_lena);
+h_lena = numel(huffmanCode(q_lena))/numel(q_lena); %q_lena in huffman code
+
+%sizeMap = containers.Map('KeyType','double', 'ValueType','any');
+%for row = 1:size(dict,1)
+%    keyCell = dict(row,1);
+%    valueCell = dict(row,2);
+%    code = valueCell{1};
+%    key = keyCell{1};
+%    sizeMap(key) = size(code,2);
+%end
+
+%SizeMap holds the lengths of all the symbols
+% TODO subtitute the symbols by their length and add all the elements
+% of the matrix, then divide by the nuelem of the matrix
+entropies = [e_sub11, e_sub12, e_sub13, e_sub14, e_sub21, e_sub22, e_sub23, e_lena;
+            h_sub11, h_sub12, h_sub13, h_sub14, h_sub21, h_sub22, h_sub23, h_lena]';
+ 
 entropies
 
+%%%%%%%% SYNTHESIS %%%%%%%%%%%%%
+dq_lena(1:128,1:128) = q_lena(1:128,1:128);
+dq_lena(1:128,129:256) = dequantize_matrix(q_lena(1:128,129:256),7,8);
+dq_lena(129:256,1:128) = dequantize_matrix(q_lena(129:256,1:128),7,8);
+dq_lena(129:256,129:256) = dequantize_matrix(q_lena(129:256,129:256),6,8);
+dq_lena(1:256,257:512) = dequantize_matrix(q_lena(1:256,257:512),5,8);
+dq_lena(257:512,1:256) = dequantize_matrix(q_lena(257:512,1:256),5,8);
+dq_lena(257:512,257:512) = dequantize_matrix(q_lena(257:512,257:512),4,8);
 
+synth_lena = haar_reverse_multilevel(dq_lena,2);
+imshow(synth_lena)
+
+%%%%%%%% PSNR calculation %%%%%%%%
+[PSNR,MSE,MAXERR,L2RAT] = measerr(lena_gray_512,synth_lena)
+% measerr already provides the PSNR but we can calculate it again using the
+% formula: 10 * log10((255^2)/MSE)
+PSNR_2 = 10 * log10((255^2)/MSE);
+% Which gives the exact same result
+[PSNR, PSNR_2]
+
+% END OF FILE
--- Codec/huffmanCode.m
+++ Codec/huffmanCode.m
@@ -1,7 +1,8 @@
-function dict = huffmanDict(matrix)
+function code = huffmanCode(matrix)
     a = unique(matrix);
     count = numel(matrix);
     out = [a,histc(matrix(:),a)/count];
     p = out(:,2);
     dict = huffmandict(a,p);
+    code = huffmanenco(matrix(:),dict);
 end
